99db7725565ef70a748ee3b7e4e4906062807e50
The-77 <62996835+The-77@users.noreply.github.com>
2020-04-01 14:49:48 +0100

    Update betterscheduler.py
    Fixing it so it writes directly to a file rather than printing in the IDE

--- betterscheduler.py
+++ betterscheduler.py
@@ -4,23 +4,29 @@ import praw
 import csv
 
 def scrape_rfl_scores():
-    reddit = praw.Reddit(client_id='nope',
+    reddit = praw.Reddit(client_id='nope', #input the correct bits of info here for it to run
                          client_secret='nope',
                          password='nope',
                          user_agent='nope',
                          username='PelotonMod')
 
-    submission = reddit.submission("fekwtl")
+    submission = reddit.submission("fekwtl") #the six characters after https://www.reddit.com/r/subreddit/comments/
 
     submission.comments.replace_more(limit=0)
     for top_level_comment in submission.comments:
-        with open('paris_nice_picks.csv', 'wb+') as csvfile:
-            pickswriter = csv.writer(csvfile, delimiter =' ')
-            pickswriter.writerows(top_level_comment.author),
-            pickswriter.writerows(top_level_comment.body)
+        with open('paris_nice_picks.csv', 'a+', encoding='utf-8', newline='') as csvfile: #choose 'something_picks.csv' as your name
+            pickswriter = csv.writer(csvfile, dialect='excel', delimiter =' ')
+            pickswriter.writerow(top_level_comment.author.name),
+            pickswriter.writerow(top_level_comment.body)
+            print(top_level_comment.author) #prints a list of authors who submitted on time
 
-schedule.every().wednesday.at("12:59").do(scrape_rfl_scores)
+schedule.every().wednesday.at("14:11").do(scrape_rfl_scores) #time & date of the submission time in your local PC time
 
 while True:
     schedule.run_pending()
     time.sleep(1)
+    
+    
+#when opening the file, tick the other box under separated by and put " in
+#make sure to use " as the text delimiter too
+#also keep it in Unicode (UTF-8)
